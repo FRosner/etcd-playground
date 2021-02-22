946d74be9176feb92944b39dc02c9b83af3fb666
Frank Rosner <frank.rosner@datastax.com>
2021-02-22 15:54:34 +0100

    assertj

---
+++ src/test/java/de/frosner/StartTest.java
@@ -0,0 +1,15 @@
+package de.frosner;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class StartTest
+{
+
+    @Test
+    public void test() {
+        assertThat(true).isTrue();
+    }
+
+}
