f541136488201fffebaeb375dd71fb00f382c6e2
Frank Rosner <frank.rosner@datastax.com>
2021-03-06 11:37:03 +0100

    toxyproxy

--- src/test/java/de/frosner/server/NodeTest.java
+++ src/test/java/de/frosner/server/NodeTest.java
@@ -5,13 +5,27 @@ import java.net.URI;
 import java.util.List;
 import java.util.Set;
 import org.awaitility.Awaitility;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.Network;
+import org.testcontainers.containers.ToxiproxyContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @Testcontainers
 class NodeTest {
+
+  private static final Network network = Network.newNetwork();
+  private static final int ETCD_PORT = 2379;
+
+  private ToxiproxyContainer.ContainerProxy etcdProxy;
+
+  @AfterAll
+  private static void afterAll() {
+    network.close();
+  }
 
   @Container
   private static final GenericContainer<?> etcd =
@@ -20,11 +34,23 @@ class NodeTest {
               "-listen-client-urls", "http://0.0.0.0:2379",
               "--advertise-client-urls", "http://0.0.0.0:2379",
               "--name", NodeTest.class.getSimpleName())
-          .withExposedPorts(2379);
+          .withExposedPorts(ETCD_PORT)
+          .withNetwork(network);
+
+  @Container
+  public static final ToxiproxyContainer toxiproxy = new ToxiproxyContainer(
+      "shopify/toxiproxy:2.1.0")
+      .withNetwork(network)
+      .withNetworkAliases("toxiproxy");
+
+  @BeforeEach
+  public void beforeEach() {
+    etcdProxy = toxiproxy.getProxy(etcd, ETCD_PORT);
+  }
 
   private List<URI> getClientEndpoints() {
     return List.of(URI.create(
-        "https://" + etcd.getContainerIpAddress() + ":" + etcd.getMappedPort(2379)));
+        "https://" + etcdProxy.getContainerIpAddress() + ":" + etcdProxy.getProxyPort()));
   }
 
   @Test
