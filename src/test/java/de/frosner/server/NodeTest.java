045d3f873e33f2b557324815d8273b74e67c0b0a
Frank Rosner <frank.rosner@datastax.com>
2021-03-06 11:27:29 +0100

    use testcontainers directly

--- src/test/java/de/frosner/server/NodeTest.java
+++ src/test/java/de/frosner/server/NodeTest.java
@@ -1,23 +1,37 @@
 package de.frosner.server;
 
-import io.etcd.jetcd.test.EtcdClusterExtension;
+import io.etcd.jetcd.launcher.EtcdContainer;
+import java.net.URI;
 import java.util.List;
 import java.util.Set;
 import org.awaitility.Awaitility;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.RegisterExtension;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
+@Testcontainers
 class NodeTest {
 
-  @RegisterExtension
-  public static final EtcdClusterExtension etcdCluster = new EtcdClusterExtension(
-      NodeTest.class.getSimpleName(), 1);
+  @Container
+  private static final GenericContainer<?> etcd =
+      new GenericContainer<>(EtcdContainer.ETCD_DOCKER_IMAGE_NAME)
+          .withCommand("etcd",
+              "-listen-client-urls", "http://0.0.0.0:2379",
+              "--advertise-client-urls", "http://0.0.0.0:2379",
+              "--name", NodeTest.class.getSimpleName())
+          .withExposedPorts(2379);
+
+  private List<URI> getClientEndpoints() {
+    return List.of(URI.create(
+        "https://" + etcd.getContainerIpAddress() + ":" + etcd.getMappedPort(2379)));
+  }
 
   @Test
   public void testTwoNodesJoinLeave() throws Exception {
-    try (Node node1 = new Node(etcdCluster.getClientEndpoints())) {
+    try (Node node1 = new Node(getClientEndpoints())) {
       node1.join();
-      try (Node node2 = new Node(etcdCluster.getClientEndpoints())) {
+      try (Node node2 = new Node(getClientEndpoints())) {
         node2.join();
 
         Awaitility.await("Node 1 to see all nodes")
